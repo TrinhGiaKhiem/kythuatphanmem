69034eb7e8bc9d86825964cd1c68e321ad3e7c28
Khiem <trinhgiakhiem0112@gmail.com>
2023-06-21 18:36:54 +0700

    tai thu len phat

---
+++ test/bai11/bai11/FileHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai11
+{
+    internal class FileHelper
+    {
+        private static object locker = new object();
+        public static string ReadFile(string filepath)
+        {
+            lock (locker)
+            {
+                if (!File.Exists(filepath))
+                    return "";
+                return File.ReadAllText(filepath).Trim();
+            }
+        }
+        public static void WriteAppendToFile(string filepath, string text)
+        {
+            lock (locker)
+            {
+                using (FileStream stream = new FileStream(filepath, FileMode.Append, FileAccess.Write, FileShare.Read))
+                {
+                    using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
+                    {
+                        writer.Write(text + Environment.NewLine);
+                    }
+                }
+
+            }
+        }
+    }
+}
